ebb93802322e17644a252bf7767bdb56b6dd0611
Arun Sundaram Sekar <asekar3@asiapac.globalcsc.net>
2017-06-06 10:38:04 +0530

    Test Commit

---
+++ basic/src/main/java/com/arun/java8/basic/App.java
@@ -0,0 +1,13 @@
+package com.arun.java8.basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
